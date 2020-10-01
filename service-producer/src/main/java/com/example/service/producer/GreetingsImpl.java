cbc4aebd0fd5e524a8e46c26c31b6d4178146020
hgwz <aqie@outlook.com>
2020-10-01 11:13:10 +0800

    Update GreetingsImpl.java

--- service-producer/src/main/java/com/example/service/producer/GreetingsImpl.java
+++ service-producer/src/main/java/com/example/service/producer/GreetingsImpl.java
@@ -3,7 +3,7 @@ package com.example.service.producer;
 import com.example.service.Greetings;
 
 /**
- * Created by xushi on 9/30/20.
+ * Created by yuli on 9/30/20.
  */
 public class GreetingsImpl implements Greetings {
     public String say(String name) {
